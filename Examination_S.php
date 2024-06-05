64c628777e59f8b59648bf4be0c0aa950df7a3bd
Zhiying <melody9306@gmail.com>
2024-06-05 11:39:30 +0800

    update Examination

--- Examination_S.php
+++ Examination_S.php
@@ -11,15 +11,15 @@ foreach ($_POST as $key => $value) {
 }
 
 $sql = "SELECT id, question, selOne, selTwo, selThree, selFour FROM selection ORDER BY RAND() LIMIT 15";
-echo '<table border>';
-echo '<tr bgcolor="green" align="center"><td>題號</td><td>問題</td><td>你的答案</td></tr>';
+echo '<table>';
 $questionNumber = 6;
 if ($result = mysqli_query($link, $sql)) {
     while ($row = mysqli_fetch_assoc($result)) {
         echo '<tr>';
-        echo '<td>' . $questionNumber . '</td>';
-        echo '<td>' . $row["question"] . '</td>';
-        echo '<td>';
+        echo '<td>' . $questionNumber . '. ' . $row["question"] . '</td>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td style=padding-bottom:2%;>';
         echo '<input type="radio" name="answer' . $row["id"] . '" value="A">' . $row["selOne"] . ' ';
         echo '<input type="radio" name="answer' . $row["id"] . '" value="B">' . $row["selTwo"] . ' ';
         echo '<input type="radio" name="answer' . $row["id"] . '" value="C">' . $row["selThree"] . ' ';
--- Examination_TF.php
+++ Examination_TF.php
@@ -6,15 +6,16 @@ $sql = "SELECT * FROM truefalse ORDER BY RAND() LIMIT 5";
 	echo '<h1>是非題</h1>';
 	// echo '<p>您的ID是：' . $user_id . '</p>'; 
 	echo '<form method="post" action="Examination_S.php">';
-	echo '<table border>';
-	echo '<tr bgcolor="green" align="center"><td>ID</td><td>問題</td><td>你的答案</td></tr>';
+	echo '<table>';
+	// echo '<tr bgcolor="green" align="center"><td>ID</td><td>問題</td><td>你的答案</td></tr>';
     $questionNumber = 1;
 	if ($result = mysqli_query($link, $sql)) {
 		while ($row = mysqli_fetch_assoc($result)) {
-			echo '<tr>';
-			echo '<td>' . $questionNumber . '</td>'; // Display the question ID
-			echo '<td>' . $row["question"] . '</td>';
-			echo '<td>';
+            echo '<tr>';
+            echo '<td>' . $questionNumber . '. ' . $row["question"] . '</td>';
+            echo '</tr>';
+            echo '<tr>';
+            echo '<td style=padding-bottom:2%;>';
 			echo '<input type="radio" name="answer' . $row["id"] . '" value="Y"> True ';
 			echo '<input type="radio" name="answer' . $row["id"] . '" value="N"> False';
 			echo '</td>';
